bba9a4b88bfc41b6ae3c2f6fe0cfe17963311848
MChandraR <2201020103@student.umrah.ac.id>
2024-11-21 00:53:46 +0700

    Added login route for testing

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,19 @@
-from flask import render_template
+from flask import render_template, request, jsonify
+import json
 
 def index():
     return render_template('index.html')
+
+def login():
+    body = request.get_json()
+    if body["username"] == "admin" and body["password"] == "1234":
+        return jsonify({
+            "status" : 200,
+            "message" : "Berhasil login !",
+            "data" : None
+        })
+    return jsonify({
+            "status" : 401,
+            "message" : "Gagal login !",
+            "data" : None
+        })
--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,4 @@ from . import controller as ct
 class Routes:
     def __init__(self, app):
         app.add_url_rule("/",'home', ct.index)
+        app.add_url_rule("/login",'login', ct.login, methods=["POST"])
