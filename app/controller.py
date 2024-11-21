459c7aa62b57061e9ddea0b949390d9e886b9610
MChandraR <2201020103@student.umrah.ac.id>
2024-11-21 15:17:24 +0700

    UI Update for Menu Hoem and Register

--- app/controller.py
+++ app/controller.py
@@ -1,12 +1,37 @@
 from flask import render_template, request, jsonify
 import json
+import pymongo
+import os
+import dotenv
+from pymongo import MongoClient
+
+dotenv.load_dotenv()
+dbuser = os.environ.get("DBUSER","")
+dbpass = os.environ.get("DBPASS","")
 
 def index():
     return render_template('index.html')
 
+def regist():
+    return render_template('register.html')
+
 def login():
-    body = request.get_json()
-    if body["username"] == "admin" and body["password"] == "1234":
+    #body = request.get_json()
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    try:
+        uri = f"mongodb+srv://{dbuser}:{dbpass}@mydb.rvfulzg.mongodb.net/?retryWrites=true&w=majority&appName=myDB"
+        client = MongoClient(uri)
+        database = client["baba"]
+        collection = database["users"].find_one({"username" : email, "password" : password})
+        print(collection)
+        client.close()
+    except Exception as e:
+        raise Exception(
+            "The following error occurred: ", e)
+        
+    if collection is not None and len(collection) > 0:
         return jsonify({
             "status" : 200,
             "message" : "Berhasil login !",
--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,5 @@ from . import controller as ct
 class Routes:
     def __init__(self, app):
         app.add_url_rule("/",'home', ct.index)
+        app.add_url_rule("/register",'regist', ct.regist)
         app.add_url_rule("/login",'login', ct.login, methods=["POST"])
