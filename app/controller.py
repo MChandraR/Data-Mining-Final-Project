3865543d4b3f43e1ce48a34010b02fab004448c6
MChandraR <2201020103@student.umrah.ac.id>
2024-11-21 23:38:51 +0700

    Input View Added & API Updated

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, Response
 import json
 import pymongo
 import os
@@ -14,6 +14,74 @@ def index():
 
 def regist():
     return render_template('register.html')
+
+def getPenjualan():
+    try:
+        uri = f"mongodb+srv://{dbuser}:{dbpass}@mydb.rvfulzg.mongodb.net/?retryWrites=true&w=majority&appName=myDB"
+        client = MongoClient(uri)
+        database = client["baba"]
+        collection = database["penjualan"].find().sort('waktu', pymongo.DESCENDING)
+        print(collection)
+        client.close()
+        data = []
+        for res in collection:
+            data.append({
+                "_id" : str(res["_id"]),
+                "aroma" : res["aroma"],
+                "waktu" : res["waktu"],
+                "jumlah" : res["jumlah"]
+            })        
+
+        print(data)
+        
+        return jsonify({
+            "status" : 200,
+            "message" : "Berhasil mengambil data !",
+            "data" : data
+        })
+    
+    
+    except Exception as e:
+        return jsonify({
+            "status" : 201,
+            "message" : "Gagal mengambil data !" + e,
+            "data" : None
+        })
+
+
+def input():
+    if request.method=="GET": return render_template('input.html')
+    
+    payload = request.get_json()
+    
+    if(payload["aroma"] == "" or payload["waktu"] == "" or payload["jumlah"] == ""):
+        return jsonify({
+            "status" : 201,
+            "message" : "Gagal : Harap periksa kembali data !",
+            "data" :payload
+        })
+
+    
+    try:
+        uri = f"mongodb+srv://{dbuser}:{dbpass}@mydb.rvfulzg.mongodb.net/?retryWrites=true&w=majority&appName=myDB"
+        client = MongoClient(uri)
+        database = client["baba"]
+        collection = database["penjualan"].insert_one({
+            "aroma" : payload["aroma"],
+            "waktu" : payload["waktu"],
+            "jumlah" : payload["jumlah"],
+        })
+        print(collection)
+        client.close()
+    except Exception as e:
+        raise Exception(
+            "The following error occurred: ", e)
+    
+    return jsonify({
+        "status" : 200,
+        "message" : "Berhasil",
+        "data" :payload
+    })
 
 def login():
     #body = request.get_json()
--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,6 @@ class Routes:
     def __init__(self, app):
         app.add_url_rule("/",'home', ct.index)
         app.add_url_rule("/register",'regist', ct.regist)
+        app.add_url_rule("/input",'input', ct.input, methods=["POST", "GET"])
         app.add_url_rule("/login",'login', ct.login, methods=["POST"])
+        app.add_url_rule("/penjualan",'penjualan', ct.getPenjualan, methods=["GET"])
